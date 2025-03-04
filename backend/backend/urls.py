b117577f0e1c5e6013769e91e7a85655fca0d6c8
ahmad <aza29@mail.aub.edu>
2025-03-04 02:32:28 +0200

    Added crops directory

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("model/", include("model.urls")),
+    path("crops/", include("crops.urls")),
 ]
