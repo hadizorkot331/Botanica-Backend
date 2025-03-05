67a1b6dcce78d3ee5c2f9c4154357351698309b5
Hadi Zorkot <hadizorkot.hadi@gmail.com>
2025-03-05 10:51:29 +0200

    Added token refresh view

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -5,4 +5,5 @@ from .views import CustomTokenObtainPairView, UserCreateAPIView
 urlpatterns = [
     path("register/", UserCreateAPIView.as_view(), name="register"),
     path("login/", CustomTokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
