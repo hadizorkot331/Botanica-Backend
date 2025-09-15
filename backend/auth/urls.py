d9a914601b39ad0d0d23b9949e0b508135c9c99e
MarcAbouSerhal <marcabouserhal@gmail.com>
2025-09-15 17:43:06 +0300

    Added backend code (:p)

--- backend/auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls.conf import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CustomTokenObtainPairView, UserCreateAPIView
-
-urlpatterns = [
-    path("register/", UserCreateAPIView.as_view(), name="register"),
-    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
-]
